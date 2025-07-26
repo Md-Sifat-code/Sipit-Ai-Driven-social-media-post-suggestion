444264209fa6d3b6e676a8421ff7f4b8e262c876
Hasan Mahadi <hasanmahadi@bdCallings-Mac-mini-3.local>
2025-07-27 01:27:20 +0600

    done

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 
-
 import NotFound from "../pages/NotFound";
 
 import Home from "../pages/Home";
@@ -9,6 +8,8 @@ import DashboardLayout from "../Layout/DashboardLayout";
 import DashboardHome from "../pages/DashboardPages/DashboardHome";
 import DashboardCommunity from "../pages/DashboardPages/DashboardCommunity";
 import DashboardPAckageandPricing from "../pages/DashboardPages/DashboardPAckageandPricing";
+import DasboardDisclaimer from "../pages/DashboardPages/DasboardDisclaimer";
+import DashboardSupportCenter from "../pages/DashboardPages/DashboardSupportCenter";
 
 const routes = createBrowserRouter([
   {
@@ -16,32 +17,37 @@ const routes = createBrowserRouter([
     element: <MainLayout />,
     children: [
       {
-        path : "/",
-        element : <Home/>
-      }
-    ]
-      
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
   {
-    path : "/dashboard",
-    element : <DashboardLayout/>,
-    children : [{
-      path : "/dashboard",
-      element : <DashboardHome/>
-    },
-    {
-      path : "/dashboard/community",
-      element : <DashboardCommunity></DashboardCommunity>
-    },
-    {
-      path : "/dashboard/package",
-      element : <DashboardPAckageandPricing/>
-    },
-    {
-      path : "/dashboard/disclaimer",
-      element : <DashboardCommunity></DashboardCommunity>
-    }
-  ]
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <DashboardHome />,
+      },
+      {
+        path: "/dashboard/community",
+        element: <DashboardCommunity></DashboardCommunity>,
+      },
+      {
+        path: "/dashboard/package",
+        element: <DashboardPAckageandPricing />,
+      },
+      {
+        path: "/dashboard/disclaimer",
+
+        element: <DasboardDisclaimer />,
+      },
+      {
+        path: "/dashboard/support",
+        element: <DashboardSupportCenter />,
+      },
+    ],
   },
   {
     path: "*",
