ac43d5b0e4e06d37a645d90d634bd4bf070b9e5d
habibb2r <habibhk127@gmail.com>
2025-07-28 01:06:43 +0600

    Completed Navbar

--- src/Layout/DashboardLayout.tsx
+++ src/Layout/DashboardLayout.tsx
@@ -3,7 +3,7 @@ import LeftNavbar from "./LeftNavbar"
 
 const DashboardLayout = () => {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex min-h-screen bg-black">
       <LeftNavbar />
       <main className="flex-1 lg:ml-0 overflow-hidden">
         <div className="pt-16 lg:pt-0 px-4 lg:px-8 py-6">
